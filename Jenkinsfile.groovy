9c24d6f456d2921df9a83a63103650e1035bf5a2
paridhishekhar <57975742+paridhishekhar@users.noreply.github.com>
2022-11-23 15:46:17 +0530

    Create Jenkinsfile.groovy

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,46 @@
+def BRANCH_NAME = ''
+
+pipeline
+{
+      agent any
+      
+      options
+      {
+            // Append timestamps to the console output.
+            timestamps()
+
+            // Disable concurrent builds.
+            disableConcurrentBuilds()
+
+            // Discard old builds.     
+            buildDiscarder(logRotator(daysToKeepStr: '10', numToKeepStr: '10'))
+      }
+
+      stages
+      {
+           stage('Checkout Source')
+            {
+                  steps
+                  {
+                        deleteDir()
+
+                        checkout scm
+                  }
+            }
+
+            stage('Trigger AppCenter Build')
+            {
+                  environment
+                  {
+                        APPCENTER_API_TOKEN = credentials('client-appcenter-token')
+                       
+                  }
+                  steps
+                  {
+                       
+                        appcenter build queue --branch master --debug-logs --app developer-lighthouse-learning.com/SampleAndroidApp --token ${APPCENTER_API_TOKEN}
+                 
+                  }
+            }
+      }
+}
